a0bfbc1d50cdd0c62c890df5fe79d774cb699064
Emmility-king <olafisoyeemmanuel7@gmail.com>
2025-04-12 10:42:15 +0100

    custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { About, Blog, Footer, Hero, Navbar, Testimonials } from "./components";
+import { Pointer } from "./components/magicui/pointer";
 
 const App = () => {
 	return (
-		<div className=" bg-bgLight flex flex-col items-center justify-start w-full ">
+		<div className=" bg-bgLight flex flex-col items-center justify-start w-full">
+			<Pointer className="fill-blue" />
 			<Navbar />
 			<Hero />
 			<About />
